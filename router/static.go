8ce16b7fba5eb92cb08e039fe63b44c672b6509a
Kira <me@imkira.com>
2016-04-06 11:34:48 +0800

    0.1.8.11 fix static file path can't be filtered
    修复了经过StaticRouter的静态文件路径不能被filter过滤和更改的问题

--- router/static.go
+++ router/static.go
@@ -21,53 +21,45 @@ type StaticRouter struct {
 func (this *StaticRouter) Pass(context RouterContext) bool {
 	//只响应GET请求
 	if strings.EqualFold(context.Method(), "GET") {
-		var parts = context.RouterParts()
-		var currentPath = ""
-		var malice = false
-		for index := this.Level() + 1; index < len(parts); index++ {
-			var param = parts[index]
-			if strings.Contains(param, `\`) {
-				//param不应该包含这种分隔符
-				malice = true
-				break
-			}
-			if param != "" && param != ".." {
-				currentPath = filepath.Join(currentPath, param)
-			}
-		}
-		if malice {
-			//空文件路径
-			var sre = &StaticRouterExecutor{""}
-			context.AddRouter(this)
-			context.AddContextExecutor(sre)
-			//设置是静态路由
-			context.SetStatic(true)
-			return true
-		} else {
-			currentPath = filepath.Join(this.path, currentPath)
-			var info, err = os.Stat(currentPath)
-			if err == nil && !info.IsDir() {
-				var sre = &StaticRouterExecutor{currentPath}
-				context.AddRouter(this)
-				context.AddContextExecutor(sre)
-				//设置是静态路由
-				context.SetStatic(true)
-				return true
-			}
-		}
+		var sre = &StaticRouterExecutor{this.Level(), this.path}
+		context.AddRouter(this)
+		context.AddContextExecutor(sre)
+		//设置是静态路由
+		context.SetStatic(true)
+		return true
 	}
 	return false
 }
 
 // 静态文件执行器
 type StaticRouterExecutor struct {
-	path string //文件路径
+	level int    //路由层级
+	path  string //文件路径
 }
 
 func (this *StaticRouterExecutor) Exec(context RouterContext) {
-	if this.path != "" {
-		http.ServeFile(context.ResponseWriter(), context.Request(), this.path)
-	} else {
-		http.NotFound(context.ResponseWriter(), context.Request())
+	var parts = context.RouterParts()
+	var currentPath = ""
+	var malice = false
+	for index := this.level + 1; index < len(parts); index++ {
+		var param = parts[index]
+		if strings.Contains(param, `\`) {
+			//param不应该包含这种分隔符
+			malice = true
+			break
+		}
+		if param != "" && param != ".." {
+			currentPath = filepath.Join(currentPath, param)
+		}
 	}
+	if !malice {
+		currentPath = filepath.Join(this.path, currentPath)
+		var info, err = os.Stat(currentPath)
+		if err == nil && !info.IsDir() {
+			http.ServeFile(context.ResponseWriter(), context.Request(), currentPath)
+			return
+		}
+	}
+	//空文件路径
+	http.NotFound(context.ResponseWriter(), context.Request())
 }
