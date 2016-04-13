32b361095100442123a74218e29619ebd925805b
Kira <me@imkira.com>
2016-04-13 16:06:58 +0800

    0.1.11.14 add funcmap recip/inv
    1.增加相反数和倒数的funcmap
    2.修复mul乘法初始值为0的bug

--- funcmap.go
+++ funcmap.go
@@ -16,6 +16,10 @@ type UserFuncMap interface {
 //  addf(num ...float64) float64:浮点数加法
 //  muli(num ...int) int:整数乘法
 //  mulf(num ...float64) float64:浮点数乘法
+//  recipi(num int) float64:整数倒数
+//  recipf(num float64) float64:浮点数倒数
+//  invi(num int) int:整数相反数
+//  invf(num float64) float64:浮点数相反数
 //  tocss(s string) template.CSS:转换字符串为CSS
 //  tohtml(s string) template.HTML:转换字符串为HTML
 //  toattr(s string) template.HTMLAttr:转换字符串为HTMLAttr
@@ -51,18 +55,36 @@ func (this *CommonFunMap) FuncMap() template.FuncMap {
 			return result
 		},
 		"muli": func(nums ...int) int {
-			var result = 0
+			var result = 1
 			for _, v := range nums {
 				result *= v
 			}
 			return result
 		},
 		"mulf": func(nums ...float64) float64 {
-			var result = 0.0
+			var result = 1.0
 			for _, v := range nums {
 				result *= v
 			}
 			return result
+		},
+		"recipi": func(num int) float64 {
+			if num != 0 {
+				return 1.0 / float64(num)
+			}
+			return 0.0
+		},
+		"recipf": func(num float64) float64 {
+			if num != 0 {
+				num = 1.0 / num
+			}
+			return num
+		},
+		"invi": func(num int) int {
+			return -num
+		},
+		"invf": func(num float64) float64 {
+			return -num
 		},
 		"tocss": func(s string) template.CSS {
 			return template.CSS(s)
